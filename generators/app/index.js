b5013e52faee3b6369f8173b098c1fc045e6d82b
Roach <roachmech@me.com>
2018-06-11 20:13:14 +0200

    Disable bower

--- generators/app/index.js
+++ generators/app/index.js
@@ -10,29 +10,73 @@ module.exports = class extends Generator {
       yosay(`Welcome to the primo ${chalk.red('generator-sass-base')} generator!`)
     );
 
-    const prompts = [
-      {
-        type: 'confirm',
-        name: 'someAnswer',
-        message: 'Would you like to enable this option?',
-        default: true
-      }
-    ];
+    const prompts = [];
 
     return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
+      // To access props later use this.props.propName;
       this.props = props;
     });
   }
 
-  writing() {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
+  installingNpmDependencies() {
+    // Install ES Lint Libraries
+    this.npmInstall(
+      [
+        '@swis/eslint-config',
+        'eslint',
+        'babel-eslint',
+        'eslint-plugin-import',
+        'eslint-plugin-node',
+        'eslint-plugin-promise',
+        'eslint-plugin-standard'
+      ],
+      {
+        'save-dev': true
+      }
     );
+
+    // Install Style Lint Libraries
+    this.npmInstall(['@swis/stylelint-config', 'stylelint-order', 'stylelint-scss'], {
+      'save-dev': true
+    });
+
+    // Install SASS Libraries
+    this.npmInstall(['sass-rhythm', 'sass-rem', 'breakpoint-sass']);
+  }
+
+  writing() {
+    // ES Lint files and scripts
+    this.fs.copy(this.templatePath('.eslintrc.js'), this.destinationPath('.eslintrc.js'));
+    this.fs.extendJSON('package.json', {
+      scripts: {
+        'lint:js':
+          'cross-env NODE_ENV=production eslint --ext=.js,.vue --format=node_modules/eslint-friendly-formatter resources/assets/js/ resources/assets/__tests__/',
+        'fix:js':
+          'cross-env NODE_ENV=production eslint --ext=.js,.vue --format=node_modules/eslint-friendly-formatter --fix resources/assets/js/ resources/assets/__tests__/'
+      }
+    });
+
+    // Style Lint files and scripts
+    this.fs.copy(this.templatePath('.stylelintrc'), this.destinationPath('.stylelintrc'));
+    this.fs.extendJSON('package.json', {
+      scripts: {
+        'lint:css': 'stylelint --syntax=scss "**/*.scss" "resources/assets/js/**/*.vue"',
+        'fix:css': 'stylelint --syntax=scss --fix "**/*.scss"'
+      }
+    });
+
+    // Combine Lint scripts
+    this.fs.extendJSON('package.json', {
+      scripts: {
+        lint: 'npm run lint:css & npm run lint:js',
+        fix: 'npm run fix:css & npm run fix:js'
+      }
+    });
   }
 
   install() {
-    this.installDependencies();
+    this.installDependencies({
+      bower: false
+    });
   }
 };
